bd1899ee0dab18ec9f9ec3fd7beb770cec5f812c
kblonski02 <kblonski02@DESKTOP-00KJNCL>
2025-04-24 20:17:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace CourseProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+    }
+
+    static void Title( string? Title )
+    {
+        Console.WriteLine("Title")
+    }
+}
